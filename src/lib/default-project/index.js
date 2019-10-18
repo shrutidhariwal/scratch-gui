d78f3efae63aea515c66d06ccdd3e39b6a92e13e
shrutidhariwal <shruti.mohnot@gmail.com>
2019-10-18 01:18:59 -0400

    changed default project to dice

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -2,10 +2,13 @@ import projectData from './project-data';
 
 /* eslint-disable import/no-unresolved */
 import popWav from '!arraybuffer-loader!./83a9787d4cb6f3b7632b4ddfebf74367.wav';
-import meowWav from '!arraybuffer-loader!./83c36d806dc92327b9e7049a565c6bff.wav';
-import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./b7853f557e4426412e64bb3da6531a99.svg';
-import costume2 from '!raw-loader!./e6ddc55a6ddd9cc9d84fe0b4c21e016f.svg';
+import BoingWav from '!arraybuffer-loader!./53a3c2e27d1fb5fdb14aaf0cb41e7889.wav';
+import backdrop1 from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
+import balla from '!raw-loader!./3c6241985b581284ec191f9d1deffde8.svg';
+import ballb from '!raw-loader!./ad7dc51cafd73e8279073e33b0eab335.svg';
+import ballc from '!raw-loader!./f221a2edf87aff3615c0c003e616b31b.svg';
+import balld from '!raw-loader!./db144b2a19f4f1ab31e30d58f00447dc.svg';
+import balle from '!raw-loader!./1c44b7494dec047371f74c705f1d99fc.svg';
 /* eslint-enable import/no-unresolved */
 
 const defaultProject = translator => {
@@ -30,25 +33,40 @@ const defaultProject = translator => {
         dataFormat: 'WAV',
         data: new Uint8Array(popWav)
     }, {
-        id: '83c36d806dc92327b9e7049a565c6bff',
+        id: '53a3c2e27d1fb5fdb14aaf0cb41e7889',
         assetType: 'Sound',
         dataFormat: 'WAV',
-        data: new Uint8Array(meowWav)
+        data: new Uint8Array(BoingWav)
     }, {
         id: 'cd21514d0531fdffb22204e0ec5ed84a',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
-        data: encoder.encode(backdrop)
+        data: encoder.encode(backdrop1)
     }, {
-        id: 'b7853f557e4426412e64bb3da6531a99',
+        id: '3c6241985b581284ec191f9d1deffde8',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
-        data: encoder.encode(costume1)
+        data: encoder.encode(balla)
     }, {
-        id: 'e6ddc55a6ddd9cc9d84fe0b4c21e016f',
+        id: 'ad7dc51cafd73e8279073e33b0eab335',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
-        data: encoder.encode(costume2)
+        data: encoder.encode(ballb)
+    }, {
+        id: 'f221a2edf87aff3615c0c003e616b31b',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(ballc)
+    }, {
+        id: 'db144b2a19f4f1ab31e30d58f00447dc',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(balld)
+    }, {
+        id: '1c44b7494dec047371f74c705f1d99fc',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(balle)
     }];
 };
 
--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -1,4 +1,4 @@
-import {defineMessages} from 'react-intl';
+import { defineMessages } from 'react-intl';
 import sharedMessages from '../shared-messages';
 
 let messages = defineMessages({
@@ -14,7 +14,7 @@ let messages = defineMessages({
     }
 });
 
-messages = {...messages, ...sharedMessages};
+messages = { ...messages, ...sharedMessages };
 
 // use the default message if a translation function is not passed
 const defaultTranslator = msgObj => msgObj.defaultMessage;
@@ -27,96 +27,267 @@ const defaultTranslator = msgObj => msgObj.defaultMessage;
 const projectData = translateFunction => {
     const translator = translateFunction || defaultTranslator;
     return ({
-        targets: [
-            {
-                isStage: true,
-                name: 'Stage',
-                variables: {
-                    '`jEk@4|i[#Fk?(8x)AV.-my variable': [
-                        translator(messages.variable),
-                        0
-                    ]
+        "targets": [{
+                "isStage": true,
+                "name": "Stage",
+                "variables": {
+                    "`jEk@4|i[#Fk?(8x)AV.-my variable": ["my variable", 0]
                 },
-                lists: {},
-                broadcasts: {},
-                blocks: {},
-                currentCostume: 0,
-                costumes: [
-                    {
-                        assetId: 'cd21514d0531fdffb22204e0ec5ed84a',
-                        name: translator(messages.backdrop, {index: 1}),
-                        md5ext: 'cd21514d0531fdffb22204e0ec5ed84a.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 240,
-                        rotationCenterY: 180
-                    }
-                ],
-                sounds: [
-                    {
-                        assetId: '83a9787d4cb6f3b7632b4ddfebf74367',
-                        name: translator(messages.pop),
-                        dataFormat: 'wav',
-                        format: '',
-                        rate: 11025,
-                        sampleCount: 258,
-                        md5ext: '83a9787d4cb6f3b7632b4ddfebf74367.wav'
-                    }
-                ],
-                volume: 100
+                "lists": {},
+                "broadcasts": {},
+                "blocks": {},
+                "comments": {},
+                "currentCostume": 0,
+                "costumes": [{
+                    "assetId": "cd21514d0531fdffb22204e0ec5ed84a",
+                    "name": "backdrop1",
+                    "md5ext": "cd21514d0531fdffb22204e0ec5ed84a.svg",
+                    "dataFormat": "svg",
+                    "rotationCenterX": 240,
+                    "rotationCenterY": 180
+                }],
+                "sounds": [{
+                    "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
+                    "name": "pop",
+                    "dataFormat": "wav",
+                    "format": "",
+                    "rate": 44100,
+                    "sampleCount": 1032,
+                    "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
+                }],
+                "volume": 100,
+                "layerOrder": 0,
+                "tempo": 60,
+                "videoTransparency": 50,
+                "videoState": "on",
+                "textToSpeechLanguage": "en"
             },
             {
-                isStage: false,
-                name: translator(messages.sprite, {index: 1}),
-                variables: {},
-                lists: {},
-                broadcasts: {},
-                blocks: {},
-                currentCostume: 0,
-                costumes: [
-                    {
-                        assetId: 'b7853f557e4426412e64bb3da6531a99',
-                        name: translator(messages.costume, {index: 1}),
-                        bitmapResolution: 1,
-                        md5ext: 'b7853f557e4426412e64bb3da6531a99.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 48,
-                        rotationCenterY: 50
+                "isStage": false,
+                "name": "Ball",
+                "variables": {},
+                "lists": {},
+                "broadcasts": {},
+                "blocks": {
+                    "g6k.h-#t+OB-^dzz8^op": {
+                        "opcode": "event_whenflagclicked",
+                        "next": "$4mo?Y~4o}cble6?80Ws",
+                        "parent": null,
+                        "inputs": {},
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": true,
+                        "x": -7,
+                        "y": -67
+                    },
+                    "$4mo?Y~4o}cble6?80Ws": {
+                        "opcode": "motion_sety",
+                        "next": "uQkF=~9seWX[t}lDeDUW",
+                        "parent": "g6k.h-#t+OB-^dzz8^op",
+                        "inputs": {
+                            "Y": [1, [4, "200"]]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "uQkF=~9seWX[t}lDeDUW": {
+                        "opcode": "control_repeat",
+                        "next": null,
+                        "parent": "$4mo?Y~4o}cble6?80Ws",
+                        "inputs": {
+                            "TIMES": [1, [6, "7"]],
+                            "SUBSTACK": [2, "IKb2Mw-?};^[6}_e52O@"]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "IKb2Mw-?};^[6}_e52O@": {
+                        "opcode": "motion_gotoxy",
+                        "next": "K]9G?Y](Vr6BBj`VH#mA",
+                        "parent": "uQkF=~9seWX[t}lDeDUW",
+                        "inputs": {
+                            "X": [1, [4, "-215"]],
+                            "Y": [3, "|QTcP!gw%Q[D%N6YQ2nu", [4, "150"]]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "|QTcP!gw%Q[D%N6YQ2nu": {
+                        "opcode": "operator_subtract",
+                        "next": null,
+                        "parent": "IKb2Mw-?};^[6}_e52O@",
+                        "inputs": {
+                            "NUM1": [3, ")e1*D.`rXnaG?)AqvxLf", [4, ""]],
+                            "NUM2": [1, [4, "50"]]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    ")e1*D.`rXnaG?)AqvxLf": {
+                        "opcode": "motion_yposition",
+                        "next": null,
+                        "parent": "|QTcP!gw%Q[D%N6YQ2nu",
+                        "inputs": {},
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "K]9G?Y](Vr6BBj`VH#mA": {
+                        "opcode": "control_repeat",
+                        "next": null,
+                        "parent": "IKb2Mw-?};^[6}_e52O@",
+                        "inputs": {
+                            "TIMES": [1, [6, "10"]],
+                            "SUBSTACK": [2, "bVh@w~tK29o2|#9Pdg`f"]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "@s`gK=M|3]]~R0r4S{!I": {
+                        "opcode": "motion_movesteps",
+                        "next": null,
+                        "parent": "zia,y0r@2QfYm)H`CsPP",
+                        "inputs": {
+                            "STEPS": [1, [4, "48"]]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "bVh@w~tK29o2|#9Pdg`f": {
+                        "opcode": "chance_setCostumeProb",
+                        "next": "zia,y0r@2QfYm)H`CsPP",
+                        "parent": "K]9G?Y](Vr6BBj`VH#mA",
+                        "inputs": {
+                            "DISTRIBUTION": [1, "M;78j;QRd{4hNn1xq5|R"]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "M;78j;QRd{4hNn1xq5|R": {
+                        "opcode": "slider",
+                        "next": null,
+                        "parent": "bVh@w~tK29o2|#9Pdg`f",
+                        "inputs": {},
+                        "fields": {
+                            "SLIDER": ["33.31640082324803,1.09375,33.29376847383967,0.8376194657766954,31.45846123713561|1~2~3~4~5", null]
+                        },
+                        "shadow": true,
+                        "topLevel": false
+                    },
+                    "zia,y0r@2QfYm)H`CsPP": {
+                        "opcode": "control_create_clone_of",
+                        "next": "@s`gK=M|3]]~R0r4S{!I",
+                        "parent": "bVh@w~tK29o2|#9Pdg`f",
+                        "inputs": {
+                            "CLONE_OPTION": [1, "7@}FiHOlcy9r/c~YWOi4"]
+                        },
+                        "fields": {},
+                        "shadow": false,
+                        "topLevel": false
+                    },
+                    "7@}FiHOlcy9r/c~YWOi4": {
+                        "opcode": "control_create_clone_of_menu",
+                        "next": null,
+                        "parent": "zia,y0r@2QfYm)H`CsPP",
+                        "inputs": {},
+                        "fields": {
+                            "CLONE_OPTION": ["_myself_", null]
+                        },
+                        "shadow": true,
+                        "topLevel": false
+                    }
+                },
+                "comments": {},
+                "currentCostume": 4,
+                "costumes": [{
+                        "assetId": "3c6241985b581284ec191f9d1deffde8",
+                        "name": "ball-a",
+                        "bitmapResolution": 1,
+                        "md5ext": "3c6241985b581284ec191f9d1deffde8.svg",
+                        "dataFormat": "svg",
+                        "rotationCenterX": 22,
+                        "rotationCenterY": 22
                     },
                     {
-                        assetId: 'e6ddc55a6ddd9cc9d84fe0b4c21e016f',
-                        name: translator(messages.costume, {index: 2}),
-                        bitmapResolution: 1,
-                        md5ext: 'e6ddc55a6ddd9cc9d84fe0b4c21e016f.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 46,
-                        rotationCenterY: 53
-                    }
-                ],
-                sounds: [
+                        "assetId": "ad7dc51cafd73e8279073e33b0eab335",
+                        "name": "ball-b",
+                        "bitmapResolution": 1,
+                        "md5ext": "ad7dc51cafd73e8279073e33b0eab335.svg",
+                        "dataFormat": "svg",
+                        "rotationCenterX": 22,
+                        "rotationCenterY": 22
+                    },
                     {
-                        assetId: '83c36d806dc92327b9e7049a565c6bff',
-                        name: translator(messages.meow),
-                        dataFormat: 'wav',
-                        format: '',
-                        rate: 22050,
-                        sampleCount: 18688,
-                        md5ext: '83c36d806dc92327b9e7049a565c6bff.wav'
+                        "assetId": "f221a2edf87aff3615c0c003e616b31b",
+                        "name": "ball-c",
+                        "bitmapResolution": 1,
+                        "md5ext": "f221a2edf87aff3615c0c003e616b31b.svg",
+                        "dataFormat": "svg",
+                        "rotationCenterX": 22,
+                        "rotationCenterY": 22
+                    },
+                    {
+                        "assetId": "db144b2a19f4f1ab31e30d58f00447dc",
+                        "name": "ball-d",
+                        "bitmapResolution": 1,
+                        "md5ext": "db144b2a19f4f1ab31e30d58f00447dc.svg",
+                        "dataFormat": "svg",
+                        "rotationCenterX": 22,
+                        "rotationCenterY": 22
+                    },
+                    {
+                        "assetId": "1c44b7494dec047371f74c705f1d99fc",
+                        "name": "ball-e",
+                        "bitmapResolution": 1,
+                        "md5ext": "1c44b7494dec047371f74c705f1d99fc.svg",
+                        "dataFormat": "svg",
+                        "rotationCenterX": 22,
+                        "rotationCenterY": 22
                     }
                 ],
-                volume: 100,
-                visible: true,
-                x: 0,
-                y: 0,
-                size: 100,
-                direction: 90,
-                draggable: false,
-                rotationStyle: 'all around'
+                "sounds": [{
+                        "assetId": "53a3c2e27d1fb5fdb14aaf0cb41e7889",
+                        "name": "Boing",
+                        "dataFormat": "wav",
+                        "format": "adpcm",
+                        "rate": 22050,
+                        "sampleCount": 7113,
+                        "md5ext": "53a3c2e27d1fb5fdb14aaf0cb41e7889.wav"
+                    },
+                    {
+                        "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
+                        "name": "Pop",
+                        "dataFormat": "wav",
+                        "format": "",
+                        "rate": 44100,
+                        "sampleCount": 1032,
+                        "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
+                    }
+                ],
+                "volume": 100,
+                "layerOrder": 1,
+                "visible": true,
+                "x": 258,
+                "y": -150,
+                "size": 150,
+                "direction": 90,
+                "draggable": false,
+                "rotationStyle": "all around"
             }
         ],
-        meta: {
-            semver: '3.0.0',
-            vm: '0.1.0',
-            agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' // eslint-disable-line max-len
+        "monitors": [],
+        "extensions": ["chance"],
+        "meta": {
+            "semver": "3.0.0",
+            "vm": "0.2.0",
+            "agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
         }
     });
 };
